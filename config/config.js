acfdffafb0cad0c2d724a4394e593e8b0e6db831
Dinty1 <67452089+Dinty1@users.noreply.github.com>
2021-06-13 20:52:17 +0100

    Staff Updates

--- config/config.js
+++ config/config.js
@@ -14,13 +14,13 @@ module.exports = {
         '77deb7c8-690c-48f1-a2b1-da87d4c69d76',//kyyl_
         'b5f4e511-d13d-4c93-931b-c526a68c7c74',//skelezomperman
         '125df18c-516d-4bfe-bb04-88254ed25d00',//tom_pairs
-        'ae4e53ea-f252-4afa-89e9-deb1dad0679e',//unjinz
-        '4ab3e8f8-d259-4f36-ad72-78a756afbf1c',//vickitori_
         '4040c845-3f17-464d-ae54-4b6310c7dfb8',//vulpicula
         '6f4821b0-1996-4f3b-a776-ce0cdc8e7276',//ap_red
         'f65bc7cb-ce43-477c-baf7-1b4c72798bd0',//chiefbozx
         '7c385c95-195e-4576-8771-ec4f6f00dc57',//cynra_
         '5cc70692-7282-4fd5-8d89-11c08535bb11',//sesese9
         'ffdaf900-cdb2-4f09-a0fb-81e3087da4e7',//frumple
+        '49590c1d-3fb8-4f6c-9bdd-39351621c679',//needn_nl
+        '9d623930-b069-4cb5-94e4-81499eb2cb92',//missa_solemnis
     ]
 }
